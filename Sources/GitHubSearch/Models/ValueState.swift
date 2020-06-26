a708ded2f62a30117f4568d01043661976b2df22
Alexandre Mantovani Tavares <alexandre@live.in>
2020-06-26 21:31:43 +0200

    feat: ValueState enum

---
+++ Sources/GitHubSearch/Models/ValueState.swift
@@ -0,0 +1,20 @@
+//
+//  ValueState.swift
+//  GitHubSearch
+//
+//  Created by Alexandre Mantovani Tavares on 26/06/20.
+//
+
+import Foundation
+
+enum ValueState<Value> {
+    case idle
+    case loading
+    case loaded(Value)
+    case error(Error)
+
+    var isLoading: Bool {
+        if case .loading = self { return true }
+        return false
+    }
+}
