152a1ade7ef78636d6faefac1d08f0c0f1da7f88
Alexandre Mantovani Tavares <alexandre@live.in>
2020-06-27 21:33:46 +0200

    test: SearchParameters

---
+++ Tests/GitHubSearchTests/GitHubServiceTest.swift
@@ -0,0 +1,38 @@
+//
+//  GitHubServiceTest.swift
+//  GitHubSearchTests
+//
+//  Created by Alexandre Mantovani Tavares on 27/06/20.
+//
+
+@testable import GitHubSearch
+import Nimble
+import Quick
+import SnapshotTesting
+import XCTest
+
+class GitHubServiceTest: QuickSpec {
+    override func spec() {
+        describe("a GitHub service") {
+            context("when searching") {
+                it("should create the search queries correctly") {
+                    let parameters: [GitHubService.SearchParameters] = [
+                        .init(term: .language("someLanguage")),
+                        .init(term: .language("stuff"),
+                              page: 10,
+                              itemsPerPage: 50,
+                              sorting: .forks,
+                              order: .descending),
+                        .init(term: .language("random"),
+                              page: 15,
+                              itemsPerPage: 15,
+                              sorting: .updated,
+                              order: .ascending),
+                    ]
+
+                    assertSnapshot(matching: parameters, as: .json)
+                }
+            }
+        }
+    }
+}
